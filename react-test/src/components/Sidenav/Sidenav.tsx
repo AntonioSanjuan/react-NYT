1f1d5b1979671b633082989ec9ce2fef51f901f7
Tony Sanjuan <tonysanj13@gmail.com>
2022-02-04 15:38:21 +0100

    added ng-content angular behaviour in react

--- react-test/src/components/Sidenav/Sidenav.tsx
+++ react-test/src/components/Sidenav/Sidenav.tsx
@@ -1,6 +1,13 @@
+import useUser from '../../hooks/login/userHook';
+import { useAppSelector } from '../../hooks/state/appStateHook';
+import { selectUserIsLogged } from '../../state/user/user.selectors';
+import { Section } from '../common/section/section';
 import './Sidenav.scss'
 
 function Sidenav() {
+    const isLoggedIn = useAppSelector<boolean>(selectUserIsLogged);
+
+    const { logout } = useUser()
 
     return (
             <div className="sidenav_MainContainer">
@@ -12,14 +19,27 @@ function Sidenav() {
                             <p className="app_font_m">Search content</p>
                         </div>
                         <div className="sidenav_Section">
+                            <Section 
+                            sectionName={'Most Popular Articles'}>
+                                <i className="bi bi-bar-chart-line-fill"></i>
+                            </Section>
                         </div>
                         <div className="sidenav_Section">
+                            <Section sectionName={'to-do'}>
+                                <i className="bi bi-clock-fill"></i>
+                            </Section>
                         </div>
                     </div>
                 <div className="sidenav_FooterSection">
                     <div className="sidenav_Section">
+                        <Section sectionName={"'Contact'"}>
+                            <i className="bi bi-chat-left-dots-fill"></i>
+                        </Section>
                     </div>
-                    <div className="sidenav_Section" id="exitSection">
+                    <div style={{visibility: isLoggedIn ? 'visible': 'hidden'}} onClick={logout} className="sidenav_Section" id="exitSection">
+                        <Section sectionName={"'Exit'"}>
+                        <i className="bi bi-box-arrow-right"></i>
+                        </Section>
                     </div>
                 </div>
             </div>
--- react-test/src/components/common/section/section.tsx
+++ react-test/src/components/common/section/section.tsx
@@ -1,11 +1,18 @@
 import './section.scss'
 
-function Section() {
-    
+function Section({children, sectionName} : {children: any, sectionName: string}) {
+
     return (
         <>
         {
-            
+         <div className="Section_MainContainer">
+            <div className="Section_ImageContainer">
+                {children}
+            </div>
+            <div className="Section_DataContainer">
+                <p className="app_font_m">{sectionName}</p>
+            </div>
+        </div>   
         }
         </>
     )
