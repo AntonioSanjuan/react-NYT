6ce0b33a655cddbec635c22d03b47846c351b954
Tony Sanjuan <tonysanj13@gmail.com>
2022-03-06 12:32:44 +0100

    feature: auth guard working for /storedArticles view

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -3,43 +3,45 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './layouts/BaseLayout/BaseLayout';
 import { LoginLayout } from './layouts/LoginLayout/LoginLayout';
+import { AuthRouteGuard } from './guards/authGuard/auth.guard';
 
 const Home = React.lazy(() => import("./pages/Home/Home"));
 const Login = React.lazy(() => import("./pages/Login/Login"));
 const Contact = React.lazy(() => import("./pages/Contact/Contact"));
-const StoredArticles = React.lazy(() => import("./pages/StoredArticles/StoredArticles"))
+const StoredArticles = React.lazy(() => import("./pages/StoredArticles/StoredArticles"));
 
 function App() {
   return (
     <Routes>
-    <Route path="/" element={<Layout />}>
-
+    <Route path="/" element={
+    <Layout />}>
       <Route index element={
         <React.Suspense fallback={<>...</>}>
           <Home />
         </React.Suspense>
       } 
       />
-
       <Route
             path="contact"
             element={
-              <React.Suspense fallback={<>...</>}>
-                <Contact />
-              </React.Suspense>
+                <React.Suspense fallback={<>...</>}>
+                  <Contact />
+                </React.Suspense>
             }
           />
       <Route
             path="storedArticles"
             element={
-              <React.Suspense fallback={<>...</>}>
-                <StoredArticles />
-              </React.Suspense>
+              <AuthRouteGuard>
+                <React.Suspense fallback={<>...</>}>
+                  <StoredArticles />
+                </React.Suspense>
+              </AuthRouteGuard>
             }
           />
     </Route>
-    
-    <Route path="login" element={
+    <Route path="login" 
+    element={
     <LoginLayout />}>
       <Route index element={
         <React.Suspense fallback={<>...</>}>
---
+++ react-test/src/guards/authGuard/auth.guard.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../../hooks/state/appStateHook";
+import { selectUserIsLogged } from "../../state/user/user.selectors";
+
+export const AuthRouteGuard = ({ children }: {children: any}) => {
+    const userIsLogged = useAppSelector<boolean>(selectUserIsLogged);
+    
+    return userIsLogged ? children : <Navigate to="/" />;
+}
