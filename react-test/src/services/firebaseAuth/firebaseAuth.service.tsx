d0a4252f56659548e14019c7c72faa7c477727bb
Tony Sanjuan <tonysanj13@gmail.com>
2022-03-19 21:18:43 +0100

    added new signup service function

--- react-test/src/services/firebaseAuth/firebaseAuth.service.tsx
+++ react-test/src/services/firebaseAuth/firebaseAuth.service.tsx
@@ -1,5 +1,9 @@
-import { signInWithEmailAndPassword, signOut, UserCredential } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, UserCredential } from "firebase/auth";
 import { auth } from "../../utils/firebase.util";
+
+export function firebaseSignUp(user: string, password: string): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(auth, user, password)
+}
 
 export function firebaseLogin(user: string, password: string): Promise<UserCredential> {
     return signInWithEmailAndPassword(auth, user, password )
