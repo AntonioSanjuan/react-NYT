6f19a20b719cfeb02661bf4e8798fd706e2c3066
Tony Sanjuan <tonysanj13@gmail.com>
2022-03-19 21:19:18 +0100

    feature: added new userHook signup function

--- react-test/src/hooks/user/userHook.tsx
+++ react-test/src/hooks/user/userHook.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react"
 import { useAppDispatch } from "../state/appStateHook";
-import { firebaseLogin, firebaseLogout } from "../../services/firebaseAuth/firebaseAuth.service";
+import { firebaseLogin, firebaseLogout, firebaseSignUp } from "../../services/firebaseAuth/firebaseAuth.service";
 import { UserCredential } from "@firebase/auth";
 import { setUserAction, unsetUserAction } from "../../state/user/user.actions";
 import { auth } from "../../utils/firebase.util";
@@ -14,22 +14,31 @@ export function useUser () {
     
     const login = useCallback(async ({username, password}): Promise<UserCredential> => {
         setLoading(true);
-        console.log("auth", auth.currentUser)
         return firebaseLogin(username, password)
         .then((resp) => {
-            console.log("resp",resp.user)
-            console.log("auth", auth.currentUser)
             setLoading(false);
             setError(false);
             return resp;
         }).catch((e) => {
-            console.log("e",e)
-
             setLoading(false);
             setError(true)
             throw e;
         })
 
+    }, [])
+    
+    const signUp = useCallback(async ({username, password}): Promise<UserCredential> => {
+        setLoading(true);
+        return firebaseSignUp(username, password)
+        .then((resp) => {
+            setLoading(false);
+            setError(false);
+            return resp;
+        }).catch((e) => {
+            setLoading(false);
+            setError(true)
+            throw e;
+        });
     }, [])
     
     const logout = useCallback(async(): Promise<void> => {
