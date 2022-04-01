87f78ec66c2718b39497183bf4e043d7eb9d8ae1
Tony Sanjuan <tonysanj13@gmail.com>
2022-04-01 19:19:48 +0200

    refactor: unused elements

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './layouts/BaseLayout/BaseLayout';
 import { LoginLayout } from './layouts/LoginLayout/LoginLayout';
