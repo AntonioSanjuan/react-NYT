7736284358bff1b0c84e8da76a4aecb02e820952
Tony Sanjuan <tonysanj13@gmail.com>
2022-04-01 17:17:25 +0200

    refactor: startUp component

--- react-test/src/StartUp.tsx
+++ react-test/src/StartUp.tsx
@@ -4,28 +4,13 @@ import { useUser } from "./hooks/user/userHook";
 
 export const StartUp = ({ children }: {children: any}) => {
     const {keepUserStateUpdated, startUpLoading} = useUser();
-
+    
     useEffect(() => {
         console.log("App initialize")
         keepUserStateUpdated()
       // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [])
 
-      
-    return (
-        <>
-            { startUpLoading &&
-                <>
-                    <Loading/>
-                </>
-            }
-            { !startUpLoading &&
-                <>
-                    {children}
-                </>
-            }
-        </>
-    )
-
+      return startUpLoading ? <Loading /> : children;
 }
 
