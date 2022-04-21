ac39e4955ffaeddb23fc1d4acb23df93a4773301
Tony Sanjuan <tonysanj13@gmail.com>
2022-04-21 22:02:11 +0200

    couple of changes

--- react-test/src/components/Alert/Alert.tsx
+++ react-test/src/components/Alert/Alert.tsx
@@ -8,6 +8,7 @@ function Alert() {
   const isModalOpened = useAppSelector<boolean>(selectLayoutIsAlertOpened);
 
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       { isModalOpened
                 && (
